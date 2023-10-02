6887c56ca7d16c974baad87fc225d6225c8b095c
RobertWright0830 <dnafrankenstein@gmail.com>
2023-10-02 09:08:37 -0500

    removed unnecessary images, added forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var buttonList = document.getElementById("buttonList");
     var APIkey = "83b64cc3d3e21c19404a392f83496d54";
     var currentCard = document.querySelector(".current-card-content");
+    var forecastCards = document.querySelector(".forecast-cards");
   
     var previousSearches =
       JSON.parse(localStorage.getItem("searchEntries")) || [];
@@ -137,8 +138,41 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .then(function (data) {
           logWeatherData(data); // Log the weather data to the console
+          forecastCards.innerHTML = "";
+
+          var currentDate = dayjs();
+
+          var timestamps = Object.keys(data.list);
+
+          var selectedTimestamps = timestamps.filter((timestamp, index) => index %8 ===7);
+
+        // Loop to create 5 forecast cards
+        for (var i = 0; i < 5; i++) {
+          var timestamp = selectedTimestamps[i];
+          var forecastDate = currentDate.add(i+1, 'day').format("M/D/YYYY");
+          var icon = data.list[timestamp].weather[0].icon;
+          var tempKelvin = data.list[timestamp].main.temp;
+          var tempDisplay = ((tempKelvin - 273.15) * 9/5) + 32;
+          var windMPS = data.list[timestamp].wind.speed;
+          var windDisplay = (windMPS*2.237);
+          var humidDisplay = data.list[timestamp].main.humidity;
           
-        })
+
+          var dayCard = document.createElement("div");
+          dayCard.className = "day-card-content day-card";
+
+          // Add content to the forecast card
+          dayCard.innerHTML = `
+            <h4 class="title is-4 has-text-white">${forecastDate}</h4>
+            <img class="forecast-icon" src="http://openweathermap.org/img/w/${icon}.png">
+            <h6 >Temp: ${tempDisplay.toFixed(2)}°F</h6>
+            <h6 >Wind: ${windDisplay.toFixed(2)} MPH</h6>
+            <h6 >Humidity: ${humidDisplay} %</h6>
+          `;
+
+          forecastCards.appendChild(dayCard);
+        }
+      })
         .catch(function (error) {
           console.error("Error:", error);
         });
